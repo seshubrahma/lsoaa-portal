c64053d6d289305c4b7c55cb2989f86a5748c0ec
jchuahtacc <jchuah@tacc.utexas.edu>
2016-09-23 11:15:07 -0500

    Project skeleton

---
+++ public/js/fbinit.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGE_SENDER_ID"
+};
+
+firebase.initializeApp(config);
+
---
+++ public/js/ui.js
@@ -0,0 +1,44 @@
+function login() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth.signInWithPopup(provider).then(
+        function(result) {
+            console.log("login success!", result);
+        }
+    ).catch(
+        function(error) {
+            console.log("login failed", result);
+        }
+    );
+}
+
+function getCurrentUser() {
+    var userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('/users').child(userId).once('value').then(
+        function(snapshot) {
+            console.log("user data", snapshot.val());
+        }
+    ).catch(
+        function(error) {
+            console.log("error retrieving data", error);
+        }
+    );
+}
+
+function writeUserData() {
+    var userId = firebase.auth().currentUser.uid;
+
+    var data = {
+        name: firebase.auth().currentUser.displayName,
+        email: firebase.auth().currentUser.email
+    };
+
+    var ref = firebase.database.ref('users').child(userId);
+
+    ref.set(data);
+}
+
+function init() {
+    
+}
+
+$(document).ready(init);
