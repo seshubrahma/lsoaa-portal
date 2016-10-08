fdc6f9072424a79b5f7525e1b1c0c51e4eff1021
jchuahtacc <jchuah@tacc.utexas.edu>
2016-10-08 10:13:55 -0500

    oops forgot parentheses

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,6 +1,6 @@
 function login() {
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth.signInWithPopup(provider).then(
+    firebase.auth().signInWithPopup(provider).then(
         function(result) {
             console.log("login success!", result);
         }
